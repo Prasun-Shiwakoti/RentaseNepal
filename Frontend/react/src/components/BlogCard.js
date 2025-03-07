05aeece2c5ab78ee42447bbdf8e722362b37bb83
amritBskt <amritbskt7@gmail.com>
2025-03-07 19:32:19 +0530

    fix: blog card display issue

--- Frontend/react/src/components/BlogCard.js
+++ Frontend/react/src/components/BlogCard.js
@@ -13,9 +13,9 @@ const BlogCard = ( {blog} ) => {
     <div className="blog-card" /*onClick={handleCardClick}*/>
       <img src={blog.image} alt={blog.title} />
       <div className="blog-content">
-        <h3>{blog.summary}</h3>
+        {/* <h3>{blog.summary}</h3>
         <p>{blog.content}</p>
-        <a href={blog.link} className="read-more">Read More</a>
+        <a href={blog.link} className="read-more">Read More</a> */}
         <h3>{blog.title}</h3>
         <p>{blog.summary}</p>
         <Link to={`/blog/${blog.id}`} className="read-more">Read More</Link>
